b36465993559e1f936b465ad200d5145b1d330a4
Aftab khalil <162437958+techbodam@users.noreply.github.com>
2025-02-01 04:52:27 +0000

    update

--- Basic-javascript/variables.js
+++ Basic-javascript/variables.js
@@ -1,3 +1,13 @@
 const userId = 215010
 let userName = "Aftab Khalil"
 var userAdd = "India"
+
+userName = "Techbodam"
+userAdd="chaina"
+// userId = 22552255 const can not be changed 
+// these are the variables
+// prefer not to user var because of issue in block scope and functional scope....😅😅😅
+
+console.log([userName,userAdd,userName])
+
+console.table([userName,userAdd,userId,])
