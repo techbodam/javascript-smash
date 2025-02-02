a3c21b6e63753e9c91e2b65735068438ced9e0ce
Aftab khalil <162437958+techbodam@users.noreply.github.com>
2025-02-02 05:28:18 +0000

    update

--- Basic-javascript/variables.js
+++ Basic-javascript/variables.js
@@ -3,13 +3,13 @@ let userName = "Aftab Khalil"
 var userAdd = "India"
 let userEmail
 
-userName = "Techbodam"
-userAdd="chaina"
+userName = "Tech Aftab"
+userAdd="Bharat"
+
 // if we use any variable without give any value so it value will be undefine
 // userId = 22552255 const can not be changed 
 // these are the variables
 // prefer not to user var because of issue in block scope and functional scope....😅😅😅
 
 console.log([userName,userAdd,userName,userEmail])
-
 console.table([userName,userAdd,userId,userEmail])
